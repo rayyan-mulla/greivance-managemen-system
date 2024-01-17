6d2e9a4fe7e30bf904f9d599c37f503ec68a76f0
rayyan-mulla <rayyan.mulla@somaiya.edu>
2024-01-17 22:56:29 +0530

    If the user is already logged in, redirecting to the home page; otherwise, redirecting to the login page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:grievance_management_system/screens/home/home.dart';
 import 'package:grievance_management_system/screens/login/login.dart';
 
 void main() async {
@@ -16,7 +18,40 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Login(),
+      home: InitializerWidget(),
     );
+  }
+}
+
+class InitializerWidget extends StatefulWidget {
+  @override
+  _InitializerWidgetState createState() => _InitializerWidgetState();
+}
+
+class _InitializerWidgetState extends State<InitializerWidget> {
+  FirebaseAuth _auth;
+  User _user;
+
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _auth = FirebaseAuth.instance;
+    _user = _auth.currentUser;
+    isLoading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : _user == null
+            ? Login()
+            : Home();
   }
 }
